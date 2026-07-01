54c352d32e39ab6062fa9e213e231ad5c50f15f5
Ali127Dev <Ali127Dev@gmail.com>
2026-07-02 00:06:44 +0330

    refactor: rename otp sender

--- internal/module/notification/domain/port/sms_otp_sender.go
+++ internal/module/notification/domain/port/sms_otp_sender.go
@@ -5,6 +5,6 @@ import (
 	"github.com/TrueFlowDev/Backend/internal/module/user/domain/value_object"
 )
 
-type OtpSender interface {
+type SmsOtpSender interface {
 	Send(phone value_object.Phone, otp domain.OTP) error
 }
