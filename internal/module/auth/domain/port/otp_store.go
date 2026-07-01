8935325cbc57618b24b63e706c9714567ff481c6
Ali127Dev <Ali127Dev@gmail.com>
2026-07-01 19:25:29 +0330

    feat: add otp store port

---
+++ internal/module/auth/domain/port/otp_store.go
@@ -0,0 +1,15 @@
+package port
+
+import (
+	"context"
+	"time"
+
+	"github.com/TrueFlowDev/Backend/internal/module/auth/domain"
+	shared "github.com/TrueFlowDev/Backend/internal/shared/domain/value_object"
+)
+
+type OTPStore interface {
+	Set(ctx context.Context, key shared.Phone, value domain.OTP, ttl time.Duration) error
+	Get(ctx context.Context, key shared.Phone) (domain.OTP, error)
+	Delete(ctx context.Context, key shared.Phone) error
+}
